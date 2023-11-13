389ba311b05c4e237a1c18db6fb49cd9f3ecb654
Florian <florian@ROGER.localdomain>
2023-11-13 10:36:40 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+/** IMport des modules */
+const express = require('express')
+const cors = require('cors')
+
+/** Params API */
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+/** ROUTAGE */
+
+app.get('/', (req, res) => res.send(`Well done !!!!!!!`))
+
+app.all('*', (req, res) => res.status(501).send('What the hell are you doing'))
+
+/** START API */
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`This API is running on port ${process.env.SERVER_PORT}`)
+})
